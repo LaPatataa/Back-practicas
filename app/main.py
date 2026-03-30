c3dd31ceeb163bf80cb39e8be5f66b8a96dfe792
LaPatataa <manuelrondon25@outlook.com>
2026-03-30 10:37:34 -0500

    prueba56

--- app/main.py
+++ app/main.py
@@ -7,12 +7,24 @@ app = FastAPI(
     version="0.1.0"
 )
 
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+    "https://frontpracticas.netlify.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=[
+        "Authorization",
+        "Content-Type",
+        "Accept",
+        "Origin",
+        "ngrok-skip-browser-warning",
+    ],
 )
 
 @app.get("/health")
